db4414e7f355fe3f975e5287637140f6a527ca9e
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-24 21:01:04 +0100

    the NavMenu now caches the fetches the recipe list

--- src/layout/NavMenu.tsx
+++ src/layout/NavMenu.tsx
@@ -1,15 +1,59 @@
 import {Component} from "react";
+import {Link} from "react-router-dom";
 
-export default class NavMenu extends Component {
+const STORAGE_KEY = "recipes";
+
+export default class NavMenu extends Component<any, {recipes: string[]}> {
+
+  constructor(props: any) {
+    super(props);
+    let localRecipes = window.localStorage.getItem(STORAGE_KEY) ?? "[]";
+    this.state = {recipes: JSON.parse(localRecipes)};
+  }
+
   render() {
+    let queriedRecipe = this.props.search?.match(/recipe=([^&]*)/)?.[1];
+    queriedRecipe = queriedRecipe ? decodeURI(queriedRecipe) : undefined;
+
     return <>
       <nav className={"menu navMenu p-3"} style={{height: "100%"}}>
         <p className={"menu-label"}>Rezepte</p>
         <ul className={"menu-list"}>
-          <li><a className={"has-background-primary has-text-black"} href="/?recipe=Pizza">Pizza</a></li>
-          <li><a href="/?recipe=Pizza">Chicken Nuggets</a></li>
+          {
+            this.state.recipes.map(recipe => {
+              if (recipe === queriedRecipe) {
+                return <li key={recipe}>
+                  <Link
+                    className={"has-background-primary has-text-black"}
+                    to={`/?recipe=${encodeURI(recipe)}`}
+                  >
+                    {recipe}
+                  </Link>
+                </li>
+              }
+              return <li key={recipe}>
+                <Link to={`/?recipe=${encodeURI(recipe)}`}>
+                  {recipe}
+                </Link>
+              </li>
+            })
+          }
         </ul>
       </nav>
     </>
+  }
+
+  componentDidMount() {
+    this.fetchRecipeList()
+      .then(recipes => {
+        window.localStorage.setItem(STORAGE_KEY, JSON.stringify(recipes));
+        this.setState({recipes});
+      });
+  }
+
+  async fetchRecipeList() {
+    let response = await fetch("/_REZEPTE_/_Rezeptliste.php");
+    let raw = await response.text();
+    return raw.trim().split("\n").map(e => e.trim());
   }
 }
