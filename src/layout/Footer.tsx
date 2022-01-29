5320089afadfaa2c5a57f418f654febb23f20258
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-29 13:58:04 +0100

    updated footer with actual content

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,11 +4,41 @@ export default class Footer extends Component {
   render() {
     return <>
       <footer className={"footer has-background-black-ter has-text-white-ter"}>
-        <div className={"content has-text-centered"}>
-          <p>
-            Wow, ein Footer
-          </p>
-        </div>
+        <p className={"has-text-centered pb-4"}>
+          <b>rezepte.tts.de</b> entwickelt von <a
+            className={"has-text-white-ter"}
+            href={"https://cptpiepmatz.de"}
+            target={"_blank"}
+          >
+            <b>Tim Hesse</b>
+          </a> mit <a
+          className={"has-text-white-ter"}
+          href={"https://bulma.io"}
+          target={"_blank"}
+        >
+          <b>Bulma</b>
+        </a> und <a
+          className={"has-text-white-ter"}
+          href={"https://reactjs.org"}
+          target={"_blank"}
+        >
+          <b>React</b>
+        </a>
+        </p>
+        <div className={"menu has-text-centered"}>
+            <ul className={"menu-list"}>
+              <li>
+                <a href={"https://github.com/cptpiepmatz/rezepte.ttst.de"} target={"_blank"}>
+                  📖 Quellcode
+                </a>
+              </li>
+              <li>
+                <a href={"mailto:mail@ttst.de"}>
+                  📧 Schick 'ne Mail
+                </a>
+              </li>
+            </ul>
+          </div>
       </footer>
     </>
   }
