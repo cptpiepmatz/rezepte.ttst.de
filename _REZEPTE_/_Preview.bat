b8f06854b82b77111241d2316ed39fcd68015b4e
Stefan Hesse <m.stefan@ttst.de>
2024-03-03 21:09:04 +0100

    add: Einfacher Gugelhupf

--- _REZEPTE_/_Preview.bat
+++ _REZEPTE_/_Preview.bat
@@ -3,7 +3,8 @@ rem man kann auch über ziehen mit der rechten Maustaste eine Verknüpfung auf
 rem diese Datei erstellen und sie dann in den SendTo-Ordner verschieben.
 echo off
 echo %1
-start /i "Rezeptvorschaugeneration" ..\edit.exe img "%1"
+start /i "Rezeptvorschaugeneration" ..\edit.exe img %1
 start http://localhost:3000
 
-rem pause
+rem 
+pause
