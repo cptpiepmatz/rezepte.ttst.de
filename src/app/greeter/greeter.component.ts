9b29c78410a67c58fa06d766746cf634d634ac9a
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-10-01 17:02:56 +0200

    added greeter component

---
+++ src/app/greeter/greeter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greeter',
+  imports: [],
+  templateUrl: './greeter.component.html'
+})
+export class GreeterComponent {
+
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { GreeterComponent } from "../greeter/greeter.component";
 
 @Component({
 	selector: "layout",
-	imports: [HeaderComponent, SidebarComponent, FooterComponent],
+	imports: [HeaderComponent, SidebarComponent, FooterComponent, GreeterComponent],
 	templateUrl: "./layout.component.html",
 })
 export class LayoutComponent {}
