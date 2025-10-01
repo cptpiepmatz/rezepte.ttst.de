96704080bdcc644158d45c160e1e8354a33bba12
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-10-01 14:18:41 +0200

    installed biome and fmt

--- src/app/app_/app.ts
+++ src/app/app_/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.scss",
 })
 export class App {
-  protected title = 'rezepte.ttst.de';
+	protected title = "rezepte.ttst.de";
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app_/app.config';
-import { App } from './app_/app';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app_/app.config";
+import { App } from "./app_/app";
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
