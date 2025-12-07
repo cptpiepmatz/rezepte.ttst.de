5fd552a89c1132edc4461c83e67a516cdd5c61aa
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-12-07 17:43:11 +0100

    simplify recipe service

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -7,26 +7,15 @@ import { toSignal } from "@angular/core/rxjs-interop";
   providedIn: "root",
 })
 export class RecipeService {
-  private router = inject(Router);
   private route = inject(ActivatedRoute);
   private queryParamMap = toSignal(this.route.queryParamMap);
 
-  private readRecipe = computed(() => {
+  readonly all = recipes satisfies string[];
+
+  readonly recipeName = computed(() => {
     let params = this.queryParamMap();
     if (!params) return undefined;
     return params.get("recipe") ?? undefined;
   }) satisfies Signal<string | undefined>;
 
-  private setRecipe(recipe: string): Promise<void> {
-    return this.router.navigate([], {
-      queryParams: { recipe },
-      queryParamsHandling: "merge",
-      replaceUrl: true,
-    }).then();
-  }
-
-  readonly recipe = Object.assign(this.readRecipe, {
-    set: this.setRecipe,
-    all: recipes,
-  });
 }
