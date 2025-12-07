ec9552abbd648871d176619d1f46fdcc4d7f8836
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-12-07 20:08:57 +0100

    most of the desktop works now

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -7,16 +7,17 @@ import { toHtml } from "@dimerapp/markdown/utils";
   selector: "recipe",
   imports: [],
   templateUrl: "./recipe.component.html",
+  styleUrl: "./recipe.component.scss",
 })
 export class RecipeComponent {
   protected service = inject(RecipeService);
 
   protected preparation = resource({
     params: computed(() => this.service.recipe()?.preparation),
-    loader: async ({params: content}) => {
+    loader: async ({ params: content }) => {
       let md = new MarkdownFile(content);
       await md.process();
       return toHtml(md).contents;
-    }
+    },
   });
 }
