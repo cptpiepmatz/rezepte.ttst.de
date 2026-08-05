0beb7023f29381908b974450a1a06768e5d84e56
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2026-08-05 18:24:37 +0200

    update title per page accordingly

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,17 +1,36 @@
 import {
   type ApplicationConfig,
+  inject,
+  Injectable,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
+import { bootstrapApplication, Title } from "@angular/platform-browser";
+import {
+  provideRouter,
+  RouterStateSnapshot,
+  TitleStrategy,
+} from "@angular/router";
 import { LayoutComponent } from "./layout/layout.component";
+
+@Injectable()
+class AppTitleStrategy extends TitleStrategy {
+  private readonly title = inject(Title);
+  private readonly mainTitle = "rezepte.ttst.de";
+
+  override updateTitle(snapshot: RouterStateSnapshot): void {
+    let recipe = snapshot.root.queryParamMap.get("recipe");
+    if (!recipe) return this.title.setTitle(this.mainTitle);
+    this.title.setTitle(`${recipe} - ${this.mainTitle}`);
+  }
+}
 
 const config: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([]),
+    { provide: TitleStrategy, useClass: AppTitleStrategy },
   ],
 };
 
